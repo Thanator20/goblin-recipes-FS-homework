b9668d6496add081c8c21042d303d029e8c413e4
Tamás Ádám <doom7891@gmail.com>
2022-08-02 16:59:55 +0200

    recipes main component and recipe list main component created

--- frontend/goblin-recipe/src/app/app.module.ts
+++ frontend/goblin-recipe/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './features/main/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './features/main/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './features/main/recipes/recipes.component';
+import { RecipeListComponent } from './features/main/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/main/recipes/recipe-item/recipe-item.component';
+import { HeaderComponent } from './features/main/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
