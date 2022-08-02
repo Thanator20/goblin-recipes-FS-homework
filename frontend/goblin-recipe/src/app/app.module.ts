bbba1c397de3a17acd192ef64b860304a9174ecc
Tamás Ádám <doom7891@gmail.com>
2022-08-02 16:03:51 +0200

    added shopping list components

--- frontend/goblin-recipe/src/app/app.module.ts
+++ frontend/goblin-recipe/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './features/main/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './features/main/shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
