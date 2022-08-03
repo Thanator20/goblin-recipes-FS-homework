e713a2745f27178847b24d1bc54bbf0033f520c7
Tamás Ádám <doom7891@gmail.com>
2022-08-03 12:12:50 +0200

    created recipe list view model

--- frontend/goblin-recipe/src/app/features/main/recipes/recipe-list/recipe-list.component.ts
+++ frontend/goblin-recipe/src/app/features/main/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { recipeViewModel } from 'src/app/models/recipeViewModel';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: recipeViewModel[] = [
+    {
+      name: 'recipe',
+      description: 'test recipe',
+      imagePath:
+        'https://assets.bonappetit.com/photos/62bf35ae872a6cfbb260f286/1:1/w_1920,c_limit/0701-tj-recipe-potato-v2.jpg',
+    },
+    {
+      name: 'recipe 2',
+      description: 'test recipe 2',
+      imagePath:
+        'https://assets.bonappetit.com/photos/62bf35ae872a6cfbb260f286/1:1/w_1920,c_limit/0701-tj-recipe-potato-v2.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ frontend/goblin-recipe/src/app/models/recipeViewModel.ts
@@ -0,0 +1,5 @@
+export interface recipeViewModel {
+  name: string;
+  description: string;
+  imagePath: string;
+}
